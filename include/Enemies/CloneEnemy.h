b279441b7c7becef80a4e16eaac3a7f8140f9fdc
dragooahou <etienne.tramaux@gmail.com>
2020-11-15 11:57:22 +0100

    Added boss

---
+++ include/Enemies/CloneEnemy.h
@@ -0,0 +1,33 @@
+//
+// Created by etien on 15/11/2020.
+//
+
+#ifndef MIXGAMEJAM2020_CLONEENEMY_H
+#define MIXGAMEJAM2020_CLONEENEMY_H
+
+#include "EnemyCharacter.h"
+
+class CloneEnemy : public EnemyCharacter {
+
+    enum Phase {
+        Attack,
+        Flee
+    };
+
+    Phase phase = Attack;
+
+public:
+    CloneEnemy(sf::RenderWindow *fen, PlayerCharacter *pc);
+
+    void init() override;
+
+    void onCollide(Character *other) override;
+
+    Type getType() override;
+
+    void update(float deltatime) override;
+
+};
+
+
+#endif //MIXGAMEJAM2020_CLONEENEMY_H
